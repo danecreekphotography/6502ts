56981e52063abcd84006cd1641122549c1be3a4f
Neil Enns <info@danecreekphotography.com>
2021-02-25 18:50:18 -0800

    Lint working

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
-console.log("Hello world");
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Neil Enns. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
