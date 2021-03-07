a6fcde2b3b245ba0a64cf08f74e6d79a9d2dc1f8
Neil Enns <info@danecreekphotography.com>
2021-03-07 12:26:46 -0800

    Add flag mask and a way to set all flags at once

--- src/flags.ts
+++ src/flags.ts
@@ -3,64 +3,87 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+export enum FlagMask {
+  C = 1,
+  Z = 2,
+  I = 4,
+  D = 8,
+  B = 16,
+  Unused = 32,
+  V = 64,
+  N = 128,
+}
+
 /**
  * Flags for the 6502 CPU.
  */
 export default class Flags {
-    /**
-     * Carry
-     */
-    public C = false;
+  /**
+   * Carry
+   */
+  public C = false;
 
-    /**
-     * Zero flag
-     */
-    public Z = false;
+  /**
+   * Zero flag
+   */
+  public Z = false;
 
-    /**
-     * Interrupt disable
-     */
-    public I = false;
+  /**
+   * Interrupt disable
+   */
+  public I = false;
 
-    /**
-     * Decimal mode
-     */
-    public D = false;
+  /**
+   * Decimal mode
+   */
+  public D = false;
 
-    /**
-     * Break
-     */
-    public B = false;
+  /**
+   * Break
+   */
+  public B = false;
 
-    /**
-     * Bit 5, unused
-     */
-    public Unused = true;
+  /**
+   * Bit 5, unused
+   */
+  public Unused = true;
 
-    /**
-     * Overflow
-     */
-    public V = false;
+  /**
+   * Overflow
+   */
+  public V = false;
 
-    /**
-     * Negative
-     */
-    public N = false;
+  /**
+   * Negative
+   */
+  public N = false;
 
-    /**
-     * Returns all the status flags as a single word with each bit representing one of the flag values.
-     * Bit order:
-     * N V Unused B D I Z C
-     */
-    public get Status(): number {
-        return (+!!this.N << 7) |
-            (+!!this.V << 6) |
-            (+!!this.Unused << 5) |
-            (+!!this.B << 4) |
-            (+!!this.D << 3) |
-            (+!!this.I << 2) |
-            (+!!this.Z << 1) |
-            (+!!this.C);
-    }
+  /**
+   * Returns all the status flags as a single word with each bit representing one of the flag values.
+   * Bit order:
+   * N V Unused B D I Z C
+   */
+  public get Status(): number {
+    return (
+      (+!!this.N << 7) |
+      (+!!this.V << 6) |
+      (+!!this.Unused << 5) |
+      (+!!this.B << 4) |
+      (+!!this.D << 3) |
+      (+!!this.I << 2) |
+      (+!!this.Z << 1) |
+      +!!this.C
+    );
+  }
 
+  public set Status(flags: number) {
+    this.C = (flags & FlagMask.C) > 0;
+    this.Z = (flags & FlagMask.Z) > 0;
+    this.I = (flags & FlagMask.I) > 0;
+    this.D = (flags & FlagMask.D) > 0;
+    this.B = (flags & FlagMask.B) > 0;
+    this.Unused = (flags & FlagMask.Unused) > 0;
+    this.V = (flags & FlagMask.V) > 0;
+    this.N = (flags & FlagMask.N) > 0;
+  }
 }
--- tests/flags.test.ts
+++ tests/flags.test.ts
@@ -3,44 +3,86 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import Flags from "../src/flags";
+import Flags, { FlagMask } from "../src/flags";
 
 test("Verify Flags constructor", () => {
-    const flags = new Flags();
+  const flags = new Flags();
 
-    expect(flags.B).toBe(false);
-    expect(flags.C).toBe(false);
-    expect(flags.D).toBe(false);
-    expect(flags.I).toBe(false);
-    expect(flags.N).toBe(false);
-    expect(flags.Unused).toBe(true);
-    expect(flags.V).toBe(false);
-    expect(flags.Z).toBe(false);
-    expect(flags.Status).toBe(0b00100000);
+  expect(flags.B).toBe(false);
+  expect(flags.C).toBe(false);
+  expect(flags.D).toBe(false);
+  expect(flags.I).toBe(false);
+  expect(flags.N).toBe(false);
+  expect(flags.Unused).toBe(true);
+  expect(flags.V).toBe(false);
+  expect(flags.Z).toBe(false);
+  expect(flags.Status).toBe(0b00100000);
 });
 
 test("Verify Status property", () => {
-    const flags = new Flags();
+  const flags = new Flags();
 
-    flags.C = true;
-    expect(flags.Status).toBe(0b00100001);
+  // New flags should always have the unused status set.
+  expect(flags.Status).toBe(FlagMask.Unused);
 
-    flags.Z = true;
-    expect(flags.Status).toBe(0b00100011);
+  flags.C = true;
+  expect(flags.Status).toBe(0b00100001);
 
-    flags.I = true;
-    expect(flags.Status).toBe(0b00100111);
+  flags.Z = true;
+  expect(flags.Status).toBe(0b00100011);
 
-    flags.D = true;
-    expect(flags.Status).toBe(0b00101111);
+  flags.I = true;
+  expect(flags.Status).toBe(0b00100111);
 
-    flags.B = true;
-    expect(flags.Status).toBe(0b00111111);
+  flags.D = true;
+  expect(flags.Status).toBe(0b00101111);
 
-    flags.V = true;
-    expect(flags.Status).toBe(0b01111111);
+  flags.B = true;
+  expect(flags.Status).toBe(0b00111111);
 
-    flags.N = true;
-    expect(flags.Status).toBe(0b11111111);
+  flags.V = true;
+  expect(flags.Status).toBe(0b01111111);
 
-})
+  flags.N = true;
+  expect(flags.Status).toBe(0b11111111);
+});
+
+test("Verify set Status property", () => {
+  const flags = new Flags();
+
+  // Test each one individually
+  flags.Status = 0b00000001;
+  expect(flags.C).toBe(true);
+
+  flags.Status = 0b00000010;
+  expect(flags.Z).toBe(true);
+
+  flags.Status = 0b00000100;
+  expect(flags.I).toBe(true);
+
+  flags.Status = 0b00001000;
+  expect(flags.D).toBe(true);
+
+  flags.Status = 0b00010000;
+  expect(flags.B).toBe(true);
+
+  flags.Status = 0b00100000;
+  expect(flags.Unused).toBe(true);
+
+  flags.Status = 0b01000000;
+  expect(flags.V).toBe(true);
+
+  flags.Status = 0b10000000;
+  expect(flags.N).toBe(true);
+
+  // Try all at once
+  flags.Status = 0xff;
+  expect(flags.C).toBe(true);
+  expect(flags.Z).toBe(true);
+  expect(flags.I).toBe(true);
+  expect(flags.D).toBe(true);
+  expect(flags.B).toBe(true);
+  expect(flags.Unused).toBe(true);
+  expect(flags.V).toBe(true);
+  expect(flags.N).toBe(true);
+});
